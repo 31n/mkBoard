709e34721ab88b886e391097da4978d8d999c4f3
31n <41046238+31n@users.noreply.github.com>
2020-12-29 20:45:10 +0900

    Initial Commit

---
+++ Shared/mkBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  mkBoardApp.swift
+//  Shared
+//
+//  Created by Eizo Ishikawa on 2020/12/29.
+//
+
+import SwiftUI
+
+@main
+struct mkBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
